a53627e30c7ddc9d94fdb16e65a8097513672492
GiovanniRussetti <66553293+GiovanniRussetti@users.noreply.github.com>
2021-04-19 14:18:38 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,47 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . # # # .
+        . . . # .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . # # # .
+        . # . . .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
+    basic.pause(5000)
+})
+basic.forever(function () {
+    for (let index = 0; index < 99; index++) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(5000)
+        basic.pause(5000)
+    }
+})
